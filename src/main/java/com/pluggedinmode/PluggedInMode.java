8426bdfbfe560e864061ff85a22c19f5575fc2b4
Sam Eigen <seigen22@gmail.com>
2024-06-30 15:07:43 -0500

    basic start

--- src/main/java/com/pluggedinmode/PluggedInMode.java
+++ src/main/java/com/pluggedinmode/PluggedInMode.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.pluggedinmode;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Plugged In Mode"
 )
-public class ExamplePlugin extends Plugin
+public class PluggedInMode extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private PluggedInModeConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	PluggedInModeConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(PluggedInModeConfig.class);
 	}
 }
--- src/test/java/com/pluggedinmode/PluggedInModeTest.java
+++ src/test/java/com/pluggedinmode/PluggedInModeTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.pluggedinmode;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluggedInModeTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PluggedInMode.class);
 		RuneLite.main(args);
 	}
 }
